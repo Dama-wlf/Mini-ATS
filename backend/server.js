38d29e6196986a6ba21de99059d2c53e2b2c9588
Dama-wlf <damaanil1504@gmail.com>
2025-12-18 12:04:50 +0300

    Ajout de CORS dans le backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }
+))
 app.use(express.json());
 app.use(cookieParser());
 
